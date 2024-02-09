5f65a5e70bcc39b0e0947ac48c9694df0b624cb2
Vanessa P <nessadab@gmail.com>
2024-02-09 11:22:27 +0000

    Adding new env for password and username

--- tests/sample-app.spec.js
+++ tests/sample-app.spec.js
@@ -1,23 +1,24 @@
 import { test } from '@playwright/test';
 const { sampleAppPage } = require('../models/sample-app.model');
-
+let username = process.env.USERNAME
+let password = process.env.PASSWORD
 
 test.describe.parallel('suite', () => {
 
   test('log in success', async ({ page }) => {
     const SampleAppPage = new sampleAppPage(page);
     await SampleAppPage.navigateToSampleApp();
-    await SampleAppPage.fillUsernameField('Percival Picklepants');
-    await SampleAppPage.fillPasswordField('pwd');
+    await SampleAppPage.fillUsernameField(username);
+    await SampleAppPage.fillPasswordField(password);
     await SampleAppPage.clickLoginButton();
-    await SampleAppPage.expectedLoginTextToBe('Welcome, Percival Picklepants!');
+    await SampleAppPage.expectedLoginTextToBe(`Welcome, ${username}!`);
   });
 
   test('wrong password test', async ({ page }) => {
     const SampleAppPage = new sampleAppPage(page);
     await SampleAppPage.navigateToSampleApp();
-    await SampleAppPage.fillUsernameField('Minerva Plus');
-    await SampleAppPage.fillPasswordField('WrongPass');
+    await SampleAppPage.fillUsernameField(username);
+    await SampleAppPage.fillPasswordField(password);
     await SampleAppPage.clickLoginButton();
     await SampleAppPage.expectedLoginTextToBe('Invalid username/password');
   });
@@ -25,8 +26,8 @@ test.describe.parallel('suite', () => {
   test('no username test', async ({ page }) => {
     const SampleAppPage = new sampleAppPage(page);
     await SampleAppPage.navigateToSampleApp();
-    await SampleAppPage.fillUsernameField('');
-    await SampleAppPage.fillPasswordField('pwd');
+    await SampleAppPage.fillUsernameField(username;
+    await SampleAppPage.fillPasswordField(password);
     await SampleAppPage.clickLoginButton();
     await SampleAppPage.expectedLoginTextToBe('Invalid username/password');
   });
@@ -34,10 +35,10 @@ test.describe.parallel('suite', () => {
   test('log out test', async ({ page }) => {
     const SampleAppPage = new sampleAppPage(page);
     await SampleAppPage.navigateToSampleApp();
-    await SampleAppPage.fillUsernameField('Percival Picklepants');
-    await SampleAppPage.fillPasswordField('pwd');
+    await SampleAppPage.fillUsernameField(username);
+    await SampleAppPage.fillPasswordField(password);
     await SampleAppPage.clickLoginButton();
-    await SampleAppPage.expectedLoginTextToBe('Welcome, Percival Picklepants!');
+    await SampleAppPage.expectedLoginTextToBe(`Welcome, ${username}!`);
     await SampleAppPage.clickLogoutButton();
     await SampleAppPage.expectedLoginTextToBe('User logged out.');
   });
